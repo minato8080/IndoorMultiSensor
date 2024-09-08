1937cffab6e22819baaef60e868f33552edaecb8
Kaito Fujinuma <sokonashi0124@gmail.com>
2024-09-08 17:43:09 +0900

    BME280 module display on LCD is completed.

--- src/lcd.hpp
+++ src/lcd.hpp
@@ -1,3 +1,6 @@
+#ifndef LCD_HPP
+#define LCD_HPP
+
 #include <stdio.h>
 
 /** 先頭の文字埋めを行う
@@ -5,6 +8,45 @@
  * \param[in] width 桁数
  * \param[in] padChar 先頭埋めする文字
  * \param[in] output 出力先
- * \return 変換後の文字列
+ * \return 出力先
  */
-char *padInt(int number, int width, char padChar, char *output);
+char *padInt(int number, int width, char padChar, char *output) {
+    // バッファ
+    char format[20];
+    if (padChar == ' ') {
+        // 空白文字の場合、長さのみ指定
+        sprintf(format, "%%%dd", width);
+    } else {
+        // 文字指定がある場合
+        sprintf(format, "%%%c%dd", padChar, width);
+    }
+    // フォーマットを適用して出力
+    sprintf(output, format, number);
+    return output;
+}
+
+/** 先頭の文字埋めを行う
+ * \param[in] number 元の数字
+ * \param[in] width 桁数
+ * \param[in] precision 小数点以下桁数
+ * \param[in] padChar 先頭埋めする文字
+ * \param[in] output 出力先
+ * \return 出力先
+ */
+char *padFloat(float number, int width, int precision, char padChar,
+               char *output) {
+    // バッファ
+    char format[30]; // フォーマット文字列の最大長さを考慮
+    if (padChar == ' ') {
+        // 空白文字の場合、長さと精度のみ指定
+        sprintf(format, "%%%d.%df", width, precision);
+    } else {
+        // 文字指定がある場合
+        sprintf(format, "%%%c%d.%df", padChar, width, precision);
+    }
+    // フォーマットを適用して出力
+    sprintf(output, format, number);
+    return output;
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -3,54 +3,101 @@
 #include <RX8900RTC.h>
 #include <Wire.h>
 
+#include "../lib/bme280.hpp"
 #include "../lib/mhz19c.hpp"
 #include "lcd.hpp"
 #include "time.hpp"
 
 RX8900RTC RTC;
-LiquidCrystal lcd(1, 0, 12, 13, 14, 15);
-MHZ19C mhz19c(uart0, 16, 17);
+LiquidCrystal lcd(1, 0, 6, 7, 8, 9);
+MHZ19C mhz19c(uart0, 12, 13);
+BME280 bme280;
+
+// 温度は湿度・気圧の補正用であり気温よりやや高いため適当に補正する
+// 補正値の適正値はセンサの使用条件により異なる
+static const float TEMPERATURE_OFFSET = -1.5f;
 
 void serialCO2(LiquidCrystal lcd, MHZ19C mhz19c);
+void serialEnvironment(LiquidCrystal lcd, BME280 bme280);
 
 void setup() {
-    // picoの場合setup内で再度beginする必要がある
+    // picoの場合setup内で再度begin
     Wire.begin();
+    Wire1.begin();
 
-    // ディスプレイの行数(16)と桁数(2)
+    // LCDディスプレイ初期化
     lcd.begin(16, 2);
+    // CO2測定モジュール初期化
     mhz19c.init();
-
-    // RTC初期化
+    // 温湿度気圧測定モジュール初期化
+    bme280.init();
+    // RTCモジュール初期化
     RTC.init();
 
-    // RTC.get()よりcompileTime()が新しい時はcompiletimeをRTC.set
+    // RTCの時刻とコンパイル時刻の新しい方をセット
     time_t time_now = RTC.get();
     time_t compiletime = compileTime();
-    if (time_now < compiletime) {
+    if (time_now < compiletime)
         RTC.set(compiletime);
-    }
 
     // テスト用
     Serial.begin(9600);
 }
 
 void loop() {
-    serialTime(lcd, RTC.read());
-    // serialCO2(lcd, mhz19c);
+    // serialTime(lcd, RTC.read());
+    serialCO2(lcd, mhz19c);
+    serialEnvironment(lcd, bme280);
     delay(1000);
 }
 
+/** CO2を測定し、LCDに出力
+ * \param[in] lcd LiquidCrystalインスタンス
+ * \param[in] mhz19c MHZ19Cインスタンス
+ */
 void serialCO2(LiquidCrystal lcd, MHZ19C mhz19c) {
-    // read CO2
+    // 文字変換用のバッファ
+    char buffer[50];
+
+    // CO2数値を読み取る
     int co2;
     mhz19c.measure(&co2);
 
-    // CO2をフォーマットしてLCDに出力
-    char buffer[50];
-    lcd.setCursor(0, 0);
+    // LCDに出力
+    lcd.setCursor(9, 1);
     lcd.print(padInt(co2, 4, ' ', buffer));
-
-    lcd.setCursor(4, 0);
+    lcd.setCursor(13, 1);
     lcd.print("ppm");
+}
+
+/** 温湿度気圧を測定し、LCDに出力
+ * \param[in] lcd LiquidCrystalインスタンス
+ * \param[in] bme280 BME280インスタンス
+ */
+void serialEnvironment(LiquidCrystal lcd, BME280 bme280) {
+    // 文字変換用のバッファ
+    char buffer[50];
+    // 温度、気圧、湿度を読み取る
+    float temperature, pressure, humidity;
+    bme280.read_env(&temperature, &humidity, &pressure);
+    temperature += TEMPERATURE_OFFSET;
+
+    // LCDに出力
+    // 温度
+    lcd.setCursor(0, 0);
+    lcd.print(padFloat(temperature, 2, 1, ' ', buffer));
+    lcd.setCursor(4, 0);
+    lcd.print(char(223)); // '゜'記号
+    lcd.setCursor(5, 0);
+    lcd.print('C');
+    // 気圧
+    lcd.setCursor(7, 0);
+    lcd.print(padFloat(pressure, 4, 1, ' ', buffer));
+    lcd.setCursor(13, 0);
+    lcd.print("hPa");
+    // 湿度
+    lcd.setCursor(0, 1);
+    lcd.print(padFloat(humidity, 2, 1, ' ', buffer));
+    lcd.setCursor(4, 1);
+    lcd.print('%');
 }
